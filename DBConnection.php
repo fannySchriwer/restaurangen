4ae3cf84f6f82153cfc3a6d7a8d8abdab952097e
Fanny Schriwer <fanny_schriwer@hotmail.com>
2019-08-27 21:40:22 +0200

    added port 8889

--- DBConnection.php
+++ DBConnection.php
@@ -12,7 +12,7 @@ class Database {
         $this->connection = null;
 
         try {
-            $this->connection = new PDO("mysql:host=" . $this->host . "; dbname=" . $this->db_name, $this->username, $this->password);
+            $this->connection = new PDO("mysql:host=" . $this->host . "; port=8889; dbname=" . $this->db_name, $this->username, $this->password);
             $this->connection->exec("set names utf8");
         } catch(PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
