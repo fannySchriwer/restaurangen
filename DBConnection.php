8886a60b1ba4cb35fa6a445312c9597c84fab5f9
Ulrica Bonnerstig <u.bonnerstig@gmail.com>
2019-08-28 13:15:12 +0200

    Add function for fetching configuration

--- DBConnection.php
+++ DBConnection.php
@@ -12,8 +12,9 @@ class Database {
         $this->connection = null;
 
         try {
-            $this->connection = new PDO("mysql:host=" . $this->host . "; dbname" . $this->db_name, $this->username, $this->password);
-            $this->connection->exec("set names utf8");
+            $this->connection = new PDO("mysql:host=" . $this->host . "; dbname=" . $this->db_name, $this->username, $this->password);
+			$this->connection->exec("set names utf8");
+
         } catch(PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
         }
--- bookings/GetConfiguration.php
+++ bookings/GetConfiguration.php
@@ -1,3 +1,11 @@
 <?php
+	include_once '../DBConnection.php';
+	include_once '../classes/Configuration.php';
 
+	$db = new Database();
+	$conn = $db->getConnection();
+	$configuration = new Configuration($conn);
+	$getConfiguration = $configuration->getBookedTables();
+
+	echo(json_encode($getConfiguration));
 ?>
