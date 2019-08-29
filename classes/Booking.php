02ef9f5b5166111e8e542942fef24fe61dd7a572
Ulrica Bonnerstig <u.bonnerstig@gmail.com>
2019-08-29 14:38:47 +0200

    Implement row Class

--- classes/Booking.php
+++ classes/Booking.php
@@ -1,31 +1,30 @@
 <?php
-	class Booking {
-		private $conn;
-		private $table_name = 'bookings';
+class Booking {
+	private $conn;
+	private $table_name = 'bookings';
 
-		public function __construct($db) {
-			$this->conn = $db;
-		}
-
-		public function getBookedTables($date) {
-			$statement = $this->conn->prepare(
-				"SELECT bookings.*, booking_ID
-				FROM bookings
-                WHERE DATE(bookings.sitting) = :date ");
-				
-			$statement->execute([
-				':date' => $date
-			]);
-			$all_bookings = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-			return $all_bookings;
-		}
+	public function __construct($db) {
+		$this->conn = $db;
 	}
 
-	class BookingRow {
-		public $booking_ID;
-		public $costumer_ID;
-		public $guests;
-		public $sitting;
+	public function getBookedTables($date) {
+		$statement = $this->conn->prepare(
+			"SELECT bookings.*, booking_ID
+			FROM bookings
+			WHERE DATE(bookings.sitting) = :date ");
+			
+		$statement->execute([
+			':date' => $date
+		]);
+		$all_bookings = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+		return $all_bookings;
 	}
-?>
+}
+
+class BookingRow {
+	public $booking_ID;
+	public $costumer_ID;
+	public $guests;
+	public $sitting;
+}
