4c05cee60e410d4c1ad73651afa3b62d69724e92
Ulrica Bonnerstig <u.bonnerstig@gmail.com>
2019-08-29 11:10:29 +0200

    Implement configuration row

--- bookings/GetConfiguration.php
+++ bookings/GetConfiguration.php
@@ -1,11 +1,24 @@
 <?php
-	include_once '../DBConnection.php';
-	include_once '../classes/Configuration.php';
 
-	$db = new Database();
-	$conn = $db->getConnection();
-	$configuration = new Configuration($conn);
-	$getConfiguration = $configuration->getBookedTables();
+include_once '../DBConnection.php';
+include_once '../classes/Configuration.php';
 
-	echo(json_encode($getConfiguration));
-?>
+// instantiate database
+$database = new Database();
+$db = $database->getConnection();
+
+// instantiate configuration object/class with database
+$configuration = new Configuration($db);
+
+$getConfiguration = $configuration->getConfigurationTable();
+
+$configurationRow = new ConfigurationRow();
+
+$configurationRow->tables = $getConfiguration[0]['tables'];
+$configurationRow->sitting_one = $getConfiguration[0]['sitting_one'];
+$configurationRow->sitting_two = $getConfiguration[0]['sitting_two'];
+$configurationRow->GDPR = $getConfiguration[0]['GDPR'];
+
+echo(json_encode($configurationRow));
+
+echo(json_encode($getConfiguration));
