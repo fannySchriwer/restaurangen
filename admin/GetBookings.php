656df8001856e1fa1d6a839937ef2837fb625cb7
TomSygiel <tomasz.szczygiel@medieinstitutet.se>
2019-08-29 12:29:54 +0200

    Add get booking functionality for admin back end

--- admin/GetBookings.php
+++ admin/GetBookings.php
@@ -4,13 +4,27 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 include_once '../DBConnection.php';
-// include_once '../objects/product.php';
+include_once '../classes/Booking.php';
  
 $database = new Database();
 $db = $database->getConnection();
+
+$booking = new Booking($db);
+$bookingResults = $booking->getBookings();
+
+// check if records exist
+if(!isset ($bookingResults)) {
  
+    http_response_code(404);
+ 
+    echo json_encode(
+        array("message" => "No products found.")
+    );
+}
+else {
 
-// $product = new Product($db);
-
-
+    http_response_code(200);
+ 
+    echo json_encode($bookingResults);
+}
 ?>
--- classes/Booking.php
+++ classes/Booking.php
@@ -6,6 +6,23 @@
 		public function __construct($db) {
 			$this->conn = $db;
 		}
+
+		public function getBookings() {
+			// select all query
+			$bookingToCollect = "SELECT * FROM bookings
+				LEFT JOIN costumers
+				ON bookings.costumer_ID = costumers.costumer_ID";
+
+			// prepare query statement
+			$bookingResults = $this->conn->prepare($bookingToCollect);
+
+			// execute query
+			$bookingResults->execute();
+			
+			$all_bookings = $bookingResults->fetch(PDO::FETCH_ASSOC);
+
+			return $all_bookings;
+		}
 	}
 
 	class BookingRow {
