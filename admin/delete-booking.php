fe62e3b673c9755f24c682bb41e1c29cd27c0ed1
Fanny Schriwer <fanny_schriwer@hotmail.com>
2019-09-02 15:22:28 +0200

    changed error message

--- admin/delete-booking.php
+++ admin/delete-booking.php
@@ -3,7 +3,6 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: DELETE");
 header("Content-Type: application/json; charset=UTF-8");
-// header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
  
 // include database and object file
@@ -15,18 +14,15 @@ $database = new Database();
 $db = $database->getConnection();
 
 // Takes raw data from the request
-$json = file_get_contents('php://input');
-$data = json_decode($json);
+$data = json_decode(file_get_contents('php://input'));
 
 if(!empty($data->booking_ID)) {
     $booking_ID = $data->booking_ID;
-    var_dump($booking_ID);
     
     $booking = new Booking($db);
-    // $booking->booking_ID = $data->booking_ID;
 
     if($booking->deleteBooking($booking_ID)) {
-        echo json_encode(array('message' => 'Booking and customer was deleted successfully'));
+        echo json_encode(array('message' => 'Booking was successfully deleted'));
     }
 }
 else {
